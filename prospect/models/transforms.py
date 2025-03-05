Fixed logsfr ratio transform overflow

prospect/models/transforms.py
@@ -184,21 +184,30 @@ def dustratio_to_dust1(dust2=0.0, dust_ratio=0.0, **extras):
 # --- Transforms for the continuity non-parametric SFHs used in (Leja et al. 2018) ---
 # --------------------------------------
 
-def logsfr_ratios_to_masses(logmass=None, logsfr_ratios=None, agebins=None,
-                            **extras):
+def logsfr_ratios_to_masses(logmass=None, logsfr_ratios=None, agebins=None, **extras):
     """This converts from an array of log_10(SFR_j / SFR_{j+1}) and a value of
-    log10(\Sum_i M_i) to values of M_i.  j=0 is the most recent bin in lookback
+    log10(\Sum_i M_i) to values of M_i. j=0 is the most recent bin in lookback
     time.
+
+    This was having some overflow issues when you have tons of SFH bins. This 
+    version should be more numerically stable. 
     """
     nbins = agebins.shape[0]
-    sratios = 10**np.clip(logsfr_ratios, -10, 10)  # numerical issues...
-    #sratios = 10**np.clip(logsfr_ratios, -100, 100)  # numerical issues...
+    sratios = 10**np.clip(logsfr_ratios, -10, 10)  # Prevent numerical issues
+
     dt = (10**agebins[:, 1] - 10**agebins[:, 0])
-    coeffs = np.array([ (1. / np.prod(sratios[:i])) * (np.prod(dt[1: i+1]) / np.prod(dt[: i]))
-                        for i in range(nbins)])
+    dt_max = dt.max()
+    dt /= dt_max  # Normalize by the largest dt to improve numerical stability
+
+    coeffs = np.array([
+        (1. / np.prod(sratios[:i])) * (np.prod(dt[1:i+1]) / np.prod(dt[:i]))
+        for i in range(nbins)
+    ])
+
     m1 = (10**logmass) / coeffs.sum()
 
-    return m1 * coeffs
+    return m1 * coeffs * dt_max  # Denormalize to ensure mass conservation
+
 
 
 def logsfr_ratios_to_sfrs(logmass=None, logsfr_ratios=None, agebins=None, **extras):